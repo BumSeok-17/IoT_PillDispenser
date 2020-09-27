27eb9863b283f3c15f18b5b7af39663e19bbbcef
dorazi95 <dorazi1994@gmail.com>
2020-09-28 02:04:29 +0900

    pillapp v2

--- app/src/main/java/com/example/pillapp/SetalarmActivity.kt
+++ app/src/main/java/com/example/pillapp/SetalarmActivity.kt
@@ -1,45 +1,54 @@
 package com.example.pillapp
+
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.TimePickerDialog
 import android.content.Context
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
+import android.support.annotation.RequiresApi
 import android.support.v4.app.NotificationCompat
 import android.support.v4.app.NotificationManagerCompat
 import android.support.v7.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_setalarm.*
+import java.text.SimpleDateFormat
+import java.util.*
 
+class SetalarmActivity : AppCompatActivity() {
 
-class MainActivity : AppCompatActivity() {
-
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        init()
+        setContentView(R.layout.activity_setalarm)
 
-    }
-
-    fun init(){
-        val a:String = "Lilly3238 18mg"
-        val b:Int = 2
-        val c:Int = 6
-        val d:Int = 0
-        val e:Int = 3
-
-        tv1.text = "$a $b 개 복용"
-        tv2.text = "복용 예정 시각 : $c 시 $d 분"
-        tv3.text = "알약 보관 위치 : $e 번 보관함"
-        button2.setOnClickListener {
-            createNotificationChannel(a, b, c, d, e)
+        var disnum  = 2
+        if(intent.hasExtra("disablenum")) {
+            val disablenum: Int = intent.getIntExtra("disablenum", 0)
+            disnum = disablenum
         }
-        button1.setOnClickListener {
+        pickTimeBtn.setOnClickListener {
+            val cal = Calendar.getInstance()
+            val timeSetListener = TimePickerDialog.OnTimeSetListener { timePicker, hour, minute ->
+                cal.set(Calendar.HOUR_OF_DAY, hour)
+                cal.set(Calendar.MINUTE, minute)
+                timeTv1.text = SimpleDateFormat("HH:mm").format(cal.time) + ("에 알람이")
+                timeTv2.text = "울려요"
+                val c = cal.get(Calendar.HOUR_OF_DAY)
+                val d = cal.get(Calendar.MINUTE)
+                createNotificationChannel("Lilly3238 18mg",1,c, d, disnum )
+            }
+            TimePickerDialog(this, timeSetListener, cal.get(Calendar.HOUR_OF_DAY), cal.get(Calendar.MINUTE), true).show()
+        }
+        back.setOnClickListener {
             val intent = Intent(this, SelectActivity::class.java).apply {
-            putExtra("a", a)
+                putExtra("disablenum",disnum)
             }
             startActivity(intent)
         }
+
     }
+
     private fun createNotificationChannel(a:String, b:Int, c:Int, d:Int, e:Int) { //알람 팝업
         // Create the NotificationChannel, but only on API 26+ because
         // the NotificationChannel class is new and not in the support library
@@ -50,10 +59,10 @@ class MainActivity : AppCompatActivity() {
             val channel = NotificationChannel("channel_1", name, importance).apply {
                 description = descriptionText
             }
-        // Register the channel with the system
-        val notificationManager: NotificationManager =
-            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        notificationManager.createNotificationChannel(channel)
+            // Register the channel with the system
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
         }
         var builder = NotificationCompat.Builder(this, "channel_1")
             .setSmallIcon(R.drawable.alarm)
